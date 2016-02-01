a9842cb60fea1ee71e7dac50f4a92c4478f308c0
Rand <avelytchko@gmail.com>
2016-02-01 17:20:07 +0200

    Add shopping cart

--- db/migrate/20160131223719_add_products.rb
+++ db/migrate/20160131223719_add_products.rb
@@ -1,6 +1,16 @@
 class AddProducts < ActiveRecord::Migration
   def change
-    Product.create :title => 'Hawaiian', :description => 'This is Hawaiian pizza', :price => 100, :size => 30, :is_spicy => false, :is_veg => false, :is_best_offer => false, :path_to_image => '/images/hawaiian.jpg'
+    Product.create
+    ({
+      :title => 'Hawaiian', 
+      :description => 'This is Hawaiian pizza', 
+      :price => 100, 
+      :size => 30, 
+      :is_spicy => false, 
+      :is_veg => false, 
+      :is_best_offer => false, 
+      :path_to_image => '/images/hawaiian.jpg'
+    })
     Product.create :title => 'Pepperoni', :description => 'Nice Pepperoni pizza', :price => 90, :size => 30, :is_spicy => false, :is_veg => false, :is_best_offer => true, :path_to_image => '/images/pepperoni.jpg'
     Product.create :title => 'Vegeterian', :description => 'Vegeterian pizza without meat', :price => 125, :size => 30, :is_spicy => false, :is_veg => true, :is_best_offer => false, :path_to_image => '/images/vegeterian.jpg'
   end
--- public/app.js
+++ public/app.js
@@ -1,7 +1,12 @@
 function something()
-  {
-  	var x = window.localStorage.getItem('bbb');
-  	x = x * 1 + 1
-  	window.localStorage.setItem('bbb', x);
-    alert(x);
-  }
+{
+	var x = window.localStorage.getItem('bbb');
+	x = x * 1 + 1
+	window.localStorage.setItem('bbb', x);
+	alert(x);
+}
+
+function add_to_cart(id)
+{
+	alert('You added pizza with id: ' + id);
+}
