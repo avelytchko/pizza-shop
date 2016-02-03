263762083dda69482c022807c3fe800d33702205
Rand <avelytchko@gmail.com>
2016-02-03 21:20:31 +0200

    RuboCop changes

--- app.rb
+++ app.rb
@@ -1,4 +1,4 @@
-#encoding: utf-8
+# encoding: utf-8
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
@@ -6,8 +6,7 @@ require 'sinatra/activerecord'
 
 set :database, 'sqlite3:pizzashop.db'
 
-class Product < ActiveRecord::Base
-end
+class Product < ActiveRecord::Base; end
 
 class Order < ActiveRecord::Base
   validates :name, presence: true
@@ -32,7 +31,7 @@ post '/cart' do
   @orders_input = params[:orders]
   @items = parse_orders_input @orders_input
   if @items.length == 0
-    return erb "Your cart is empty"
+    return erb 'Your cart is empty'
   end
   @items.each do |item|
     item[0] = Product.find(item[0])
@@ -41,19 +40,19 @@ post '/cart' do
   erb :cart
 end
 
-def parse_orders_input orders_input
-  s1 = orders_input.split(",")
+def parse_orders_input(orders_input)
+  s1 = orders_input.split(',')
   arr = []
 
   s1.each do |x|
-    s2 = x.split("=")
-    s3 = s2[0].split("_")
+    s2 = x.split('=')
+    s3 = s2[0].split('_')
     id = s3[1]
     cnt = s2[1]
     arr2 = [id, cnt]
     arr.push arr2
   end
-  return arr
+  arr
 end
 
 post '/place_order' do
@@ -62,7 +61,7 @@ post '/place_order' do
     erb :order_placed
   else
     @error = @o.errors.full_messages.first
-    erb "Error"
+    erb 'Error'
   end
 end
 
